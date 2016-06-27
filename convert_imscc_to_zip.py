64f3ea78634c806354af0588c8cbe2cdd0b690fc
Rama Satya Diwakar Grandhi <diwakargrandhi@outlook.com>
2016-06-27 21:48:39 +0530

    Trying to parse the xml via ElementTree.

--- convert_imscc_to_zip.py
+++ convert_imscc_to_zip.py
@@ -4,8 +4,16 @@
 import logging
 import os
 import shutil
-import xml.etree.ElementTree as ET
 import zipfile
+import xml.etree.ElementTree as ET
+
+# Better to use the c version since it is much faster and consumes significantly less memory
+'''
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+'''
 
 # user inputs
 imscc_file_path = "/Users/rdiwakar/SLP/exports/Home-SLP-2016-LEAD-Section-1.imscc"
@@ -65,6 +73,47 @@ def unzip(src_file_path):
 		zf.extractall(dst_dir_path)
 	return dst_dir_path
 
+def get_xml_data(file_path):
+	file_obj = open(file_path)
+	file_content = ""
+	for line in file_obj:
+		file_content = file_content + line
+	logging.debug("File contents of {0}, are {1}.".format(file_path, file_content))
+	
+	# xml_data = ET.parse(file_path)
+	xml_data = file_content
+	logging.debug("Parsed xml data of {0}, are {1}.".format(file_path, xml_data))
+	
+	return xml_data
+
+def process_file(file_name, src_dir_path, dst_dir_path):
+	src_file_path = os.path.join(src_dir_path, file_name)
+	copy_file(src_file_path, dst_dir_path)
+
+def process_folder(xml_element, src_dir_path, dst_dir_path):
+	src_folder_identifier = xml_element.get('identifierref')
+	new_src_dir_path = os.path.join(src_dir_path, src_folder_identifier)
+	
+	title = xml_element.find('title').text
+	new_dst_dir_path = os.path.join(dst_dir_path, title)
+	
+	if len(xml_element.findall('item')) == 0:
+		process_file(title, new_src_dir_path, dst_dir_path)
+	else:
+		create_directory(new_dst_dir_path)
+		process_folder(new_dst_dir_path)
+
+def process_xml_data(xml_data, src_dir_path, dst_dir_path):
+	# data_root = xml_data.getroot()
+	data_root = ET.fromString(xml_data)
+	logging.debug("root: " + data_root.text)
+	organization_root = data_root.find('organizations')
+	logging.debug("organization_root: " + str(organization_root))
+	item_root = organization_root.findall(".//*[@identifier='root']")
+	logging.debug("item_root: " + str(item_root))
+	for each_item in item_root.findall('item'):
+		process_folder(each_item, src_dir_path, dst_dir_path)
+
 # Main Program Logic
 if __name__ == '__main__':
 	
@@ -90,7 +139,9 @@ if __name__ == '__main__':
 
 	imsmanifest_file_path = os.path.join(imscc_zip_file_extracted_path, imsmanifest_file_name)
 
-	xml_data = ET.parse(imsmanifest_file_path)
-	root = xml_data.getroot()
-	for child in root:
+	xml_data = get_xml_data(imsmanifest_file_path)
+	
+	process_xml_data(xml_data, imscc_zip_file_extracted_path, tmp_folder_for_structured_data_path)
+	
+
 		
